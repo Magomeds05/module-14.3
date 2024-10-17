2ec07e72d61941e2fbd29b9c814ccf6410ce0163
Magomeds05 <yusypovm@gmail.com>
2024-10-17 20:24:48 +0300

    Update module 14.3 update.py

--- module 14.3 update.py
+++ module 14.3 update.py
@@ -5,7 +5,7 @@ from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMar
 
 
 
-api = "8197570567:AAF2r-nMMyE_nqJ4Ll3dAHgXvzRn5kNVegw"
+api = ""
 bot = Bot(token=api)
 dp = Dispatcher(bot, storage=MemoryStorage())
 
